c983760331e9f42fc8505e569fd1640d3550c1d0
Dmitrii Nefedov <nefedov270399@yandex.ru>
2019-05-24 23:53:36 +0300

    v1.1.1 add variation for ran visual.kt

--- src/nefedov/turing/visual.kt
+++ src/nefedov/turing/visual.kt
@@ -3,12 +3,21 @@ package nefedov.turing
 import java.io.FileReader
 
 fun main(args: Array<String>) {
-    if (args.size != 3) {
-        System.err.println(
+
+    val (input, offset) = when (args.size) {
+        1 -> {
+            println("Input line: ")
+            val input = readLine()!!
+            input to 8
+        }
+        2 -> args[1] to 8
+        3 -> args[1] to args[2].toInt()
+        else -> return System.err.println(
             "Incorrect input. Ran as: \n" +
-                    "\tvisual <input file name> <input line for turing machine> <size of offset>"
+                    "\tvisual <input file name> [<input line for turing machine> [<size of offset>]]"
         )
+
     }
     val description = Parser.parse(FileReader(args[0]))
-    Interpreter.interpret(description, args[1], args[2].toInt())
+    Interpreter.interpret(description, input, offset)
 }
